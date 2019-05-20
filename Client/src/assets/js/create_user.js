f7ced6ab1d4f46fc1b39bc59eae57b65bcfdcffa
Babatunde <soft-sky@live.co.uk>
2019-05-20 20:11:21 +0100

    publish gh-pages

--- Client/src/assets/js/create_user.js
+++ Client/src/assets/js/create_user.js
@@ -1,4 +1,22 @@
-const createUser = (user) => {
+const getSignUpData = ()=>{
+  event.preventDefault();
+  const firstName = document.getElementById("firstname").value.trim();
+  const lastName = document.getElementById("lastname").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const confirmPassword = document.getElementById("confirmPassword").value.trim();
+  return { 
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword
+  }
+}
+const createUser = async () => {
+  event.preventDefault();
+  const user = getSignUpData();
+  console.log(user);
   const urlSignup = `https://stack-o-lite.herokuapp.com/api/v1/auth/signup`
     fetch(urlSignup, {
         method: 'POST',
@@ -10,3 +28,6 @@ const createUser = (user) => {
         console.log(result)
         })
   }
+
+
+  
--- Client/src/assets/js/login.js
+++ Client/src/assets/js/login.js
@@ -1,4 +1,15 @@
-const signIn = (loginDetails) => {
+const getSigninData = ()=>{
+  event.preventDefault();
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  return { 
+      email: email.value.trim(),
+      password: password.value.trim()
+  }
+};
+const signIn = () => {
+    event.preventDefault();
+    const loginDetails = getSigninData();
     const urlSignin = `https://stack-o-lite.herokuapp.com/api/v1/auth/Login`
     fetch(urlSignin, {
     method: 'POST',
@@ -10,3 +21,5 @@ const signIn = (loginDetails) => {
     console.log(result)
     })
   }
+
+  
