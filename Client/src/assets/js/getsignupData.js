a62e459a60524a15acb4ac0b1b0ed46eafcde31d
Babatunde <soft-sky@live.co.uk>
2019-05-20 15:08:00 +0100

    feature(signup): implement sign up feature
    -Create get user data funtion

---
+++ Client/src/assets/js/getsignupData.js
@@ -0,0 +1,22 @@
+const getSignUpData = ()=>{
+    event.preventDefault();
+    const firstname = document.getElementById("firstname");
+    const lastname = document.getElementById("lastname");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    const confirmPassword = document.getElementById("confirmPassword");
+    console.log({ 
+        firstname: firstname.value.trim(),
+        lastname: lastname.value.trim(),
+        email: email.value.trim(),
+        password: password.value.trim(),
+        confrimPassword: confirmPassword.value.trim()
+    });
+    return { 
+        firstname: firstname.value.trim(),
+        lastname: lastname.value.trim(),
+        email: email.value.trim(),
+        password: password.value.trim(),
+        confrimPassword: confirmPassword.value.trim()
+    }
+}
